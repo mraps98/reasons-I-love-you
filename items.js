2d130104ed2aa391ac867d911e3056aab5c4275a
Amritpreet Singh <amritpreet98@gmail.com>
2026-05-28 17:01:42 -0500

    add up to 51 reasons

--- items.js
+++ items.js
@@ -128,182 +128,182 @@ const loveItems = [
   {
     id: 22,
     category: "details",
-    title: "Reason 22",
-    description: "Type your personal reason here..."
+    title: "Your eyes",
+    description: "Your gorgeous eyes really are made from the stars with extravagant, heavenly beauty that has captured my heart and hypnotized me."
   },
   {
     id: 23,
     category: "memories",
-    title: "Reason 23",
-    description: "Type your personal reason here..."
+    title: "Our walks in the park",
+    description: "Our afternoon walks in the park. Be it spring, summer or fall, getting a breath of fresh air with you, swinging, cycling, is a pleasure."
   },
   {
     id: 24,
     category: "future",
-    title: "Reason 24",
-    description: "Type your personal reason here..."
+    title: "Your patience",
+    description: "Your patience, and knowing when to make the moves helps us in finance, education, work, and family."
   },
   {
     id: 25,
     category: "personality",
-    title: "Reason 25",
-    description: "Type your personal reason here..."
+    title: "Your comedy",
+    description: "You always make me laugh and it's like a breath of fresh air and completely uplifts the mood. Masti shouldn't stop."
   },
   {
     id: 26,
     category: "details",
-    title: "Reason 26",
-    description: "Type your personal reason here..."
+    title: "Your hair",
+    description: "Your beautiful, silky, soft, reshmi hair that looks straight out of shampoo commercials."
   },
   {
     id: 27,
     category: "memories",
-    title: "Reason 27",
-    description: "Type your personal reason here..."
+    title: "Our cafe dates",
+    description: "Going to cafes around the world, sipping on coffees, teas, matcha, juice, while enjoying the view and conversation. It couldn't be better."
   },
   {
     id: 28,
     category: "future",
-    title: "Reason 28",
-    description: "Type your personal reason here..."
+    title: "Your goals",
+    description: "Your goals of having, loving and nurturing a nice home in a nice city with a loving family around perfectly matches mine."
   },
   {
     id: 29,
     category: "personality",
-    title: "Reason 29",
-    description: "Type your personal reason here..."
+    title: "Your high spirit",
+    description: "You always stand face up no matter the circumstances. That is how it should be and nothing can stop you."
   },
   {
     id: 30,
     category: "details",
-    title: "Reason 30",
-    description: "Type your personal reason here..."
+    title: "Your curves",
+    description: "Cannot keep my eyes away from your curves."
   },
   {
     id: 31,
     category: "memories",
-    title: "Reason 31",
-    description: "Type your personal reason here..."
+    title: "The Farmer's market",
+    description: "Waking up early on Saturdays to dress nice, get some iced coffee and donuts with you is truly a blessing."
   },
   {
     id: 32,
     category: "future",
-    title: "Reason 32",
-    description: "Type your personal reason here..."
+    title: "Your career aspirations",
+    description: "Your goal of continuously becoming better at what you do, improving connections, gaining valuable skills and knowledge is extremely motivational."
   },
   {
     id: 33,
     category: "personality",
-    title: "Reason 33",
-    description: "Type your personal reason here..."
+    title: "Your support",
+    description: "Your unconditional, unstoppable, unmeasurable, unbeatable support to me, your family and friends is a pillar of strength in our lives."
   },
   {
     id: 34,
     category: "details",
-    title: "Reason 34",
-    description: "Type your personal reason here..."
+    title: "Your originality",
+    description: "You are you and ain't nobody like you. You are you and I love you."
   },
   {
     id: 35,
     category: "memories",
-    title: "Reason 35",
-    description: "Type your personal reason here..."
+    title: "Our time at Grand Canyon",
+    description: "Our wonderful trip to Grand Canyon and Vegas was filled with beautiful skies, scenic desert, serene drives and unforgettable memories."
   },
   {
     id: 36,
     category: "future",
-    title: "Reason 36",
-    description: "Type your personal reason here..."
+    title: "Your guidance",
+    description: "Your subtle but powerful daily guidance in my life is keeping me in the right direction and focused."
   },
   {
     id: 37,
     category: "personality",
-    title: "Reason 37",
-    description: "Type your personal reason here..."
+    title: "Your masti personality",
+    description: "Masti should never stop no matter the circumstances. This attitude of yours is a blessing."
   },
   {
     id: 38,
     category: "details",
-    title: "Reason 38",
-    description: "Type your personal reason here..."
+    title: "Your attention to details",
+    description: "When you do something, you put 100% of your mind, dedication and focus to it which is why it turns out so great."
   },
   {
     id: 39,
     category: "memories",
-    title: "Reason 39",
-    description: "Type your personal reason here..."
+    title: "Our dance sessions",
+    description: "Our never ending, varied dance practice sessions. Practicing for baby showers, rice feeding, birthdays, weddings and more is always fun with you."
   },
   {
     id: 40,
     category: "future",
-    title: "Reason 40",
-    description: "Type your personal reason here..."
+    title: "Your promise to your health",
+    description: "Your promise to eating clean, staying hydrated, working out, and healthy habits motivates you, me and your friends and family towards a strong lifestyle."
   },
   {
     id: 41,
     category: "personality",
-    title: "Reason 41",
-    description: "Type your personal reason here..."
+    title: "Your food cravings",
+    description: "Thanks to your food cravings we both get to enjoy delicious (but healthy) exotic foods ranging from east to west."
   },
   {
     id: 42,
     category: "details",
-    title: "Reason 42",
-    description: "Type your personal reason here..."
+    title: "Your spirituality",
+    description: "Your relationship to destiny, karma, and the universe."
   },
   {
     id: 43,
     category: "memories",
-    title: "Reason 43",
-    description: "Type your personal reason here..."
+    title: "Our ice cream dates",
+    description: "Our ice cream dates that range from driving-thru, eating in parking lots, dining-in with family, or bringing it home for a movie or drama."
   },
   {
     id: 44,
     category: "future",
-    title: "Reason 44",
-    description: "Type your personal reason here..."
+    title: "Your investment knowledge",
+    description: "Your knowledge on investing, diversifying stock shares, and continual financial learning."
   },
   {
     id: 45,
     category: "personality",
-    title: "Reason 45",
-    description: "Type your personal reason here..."
+    title: "Your humility",
+    description: "You have come a long way and grown stronger and improved to an unmeasurable extent. Yet you remain humble and never forget your roots."
   },
   {
     id: 46,
     category: "details",
-    title: "Reason 46",
-    description: "Type your personal reason here..."
+    title: "Your manifestation",
+    description: "Your manifest everything you truly deserve, not just for yourself but also for your family and friends and I have felt this personally."
   },
   {
     id: 47,
     category: "memories",
-    title: "Reason 47",
-    description: "Type your personal reason here..."
+    title: "Our piano times",
+    description: "Us playing piano together, be that in the campus laundry room, the Brandt hall, or at a random airport."
   },
   {
     id: 48,
     category: "future",
-    title: "Reason 48",
-    description: "Type your personal reason here..."
+    title: "Your research capabilities",
+    description: "Your unmatched ability to research trends and guide us in the right direction, be that finance, travel, events, or family decisions."
   },
   {
     id: 49,
     category: "personality",
-    title: "Reason 49",
-    description: "Type your personal reason here..."
+    title: "Your love of trying new styles",
+    description: "Trying new hairstyles, new skincare, new colognes, new outfits. We are never bored!"
   },
   {
     id: 50,
     category: "details",
-    title: "Reason 50",
-    description: "Type your personal reason here..."
+    title: "You in traditional kurti",
+    description: "Your dreamy, gorgeous beauty, paired with the traditional outfits, puts butterflies in my stomach."
   },
   {
     id: 51,
     category: "memories",
-    title: "Reason 51",
-    description: "Type your personal reason here..."
+    title: "The hammock",
+    description: "Nothing needs to be said here. That cold October night on the hammock, which was so silent yet so magical. That night changed everything."
   },
   {
     id: 52,
@@ -326,7 +326,7 @@ const loveItems = [
   {
     id: 55,
     category: "memories",
-    title: "Reason 55",
+    title: "Our karaoke fun",
     description: "Type your personal reason here..."
   },
   {
@@ -350,7 +350,7 @@ const loveItems = [
   {
     id: 59,
     category: "memories",
-    title: "Reason 59",
+    title: "Our momo nights",
     description: "Type your personal reason here..."
   },
   {
